437b0aa081f384f81a2f8671c19b3d5395fdcf8d
Tiana Aiyemo <chrissetiana@gmail.com>
2019-06-04 14:12:58 +0100

    Edit: rename package

--- app/src/test/java/com/chrissetiana/droidcafe/ExampleUnitTest.java
+++ app/src/test/java/com/chrissetiana/droidcafe/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.chrissetiana.sandwichclub;
+package com.chrissetiana.droidcafe;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
